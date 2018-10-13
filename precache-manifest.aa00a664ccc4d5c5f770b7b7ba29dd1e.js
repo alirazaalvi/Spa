942a5ed8d89452374f2dbf6424f13ee53dd35861
Ali Raza Alvi <aliraza432@gmail.com>
2018-10-13 04:16:21 +0400

    Updates

---
+++ precache-manifest.aa00a664ccc4d5c5f770b7b7ba29dd1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff9c3c0df0539fd9363c",
+    "url": "/Spa/static/js/runtime~main.ff9c3c0d.js"
+  },
+  {
+    "revision": "58402bb7da675f079c84",
+    "url": "/Spa/static/js/main.58402bb7.chunk.js"
+  },
+  {
+    "revision": "28454f394f0e573fa68c",
+    "url": "/Spa/static/js/1.28454f39.chunk.js"
+  },
+  {
+    "revision": "58402bb7da675f079c84",
+    "url": "/Spa/static/css/main.73766970.chunk.css"
+  },
+  {
+    "revision": "28454f394f0e573fa68c",
+    "url": "/Spa/static/css/1.6f34203c.chunk.css"
+  },
+  {
+    "revision": "0cc16a847a09d8d74e124bc60c36ff1c",
+    "url": "/Spa/index.html"
+  }
+];
