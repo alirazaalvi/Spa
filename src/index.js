41619139f7f15fc07a991dab3d1f444922f16b76
Ali Raza Alvi <aliraza432@gmail.com>
2019-05-14 13:24:39 +0500

    Added back the pwa code.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import Store from './stores';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Provider store={Store}><App /></Provider>, document.getElementById('root') || document.createElement('div'));
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
